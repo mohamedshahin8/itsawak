95769ef536777eeba3d190c67075bb27e80a1ee3
shahin <mohamedshahin228@gmail.com>
2018-12-13 15:43:59 +0200

    update files

--- clothesphonesapp/models.py
+++ clothesphonesapp/models.py
@@ -20,7 +20,7 @@ class Shop(models.Model):
     shop_phone      = models.CharField(max_length= 11)
     shop_location   = models.CharField(max_length= 220)
     num_of_share    = models.DecimalField(max_digits= 10 , decimal_places= 0, default= 0)
-    shop_image      = models.ImageField(upload_to = 'static' , default = None)
+    shop_image      = models.ImageField(upload_to = 'static/images' , default = None)
     class Meta:
         ordering = ['num_of_share']
 
@@ -36,7 +36,7 @@ class Product(models.Model):
     size    = models.CharField(max_length= 10 ,default= 'S' , help_text= "S,M,L,XL,XXL")
     price   = models.DecimalField(decimal_places = 2 , max_digits = 20, default= 0.00)
     describtion = models.TextField(null = True , help_text = 'this is optional if type is ACCESSORIES')
-    image = models.ImageField(upload_to = 'static', null= True)
+    image = models.ImageField(upload_to = 'static/images', null= True)
     shop_name = models.ForeignKey('Shop' , on_delete = models.CASCADE)
 
     class Meta:
