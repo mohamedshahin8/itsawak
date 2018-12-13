2e94f2c21b0c10bf9f3380043faba2f5613ba644
shahin <mohamedshahin228@gmail.com>
2018-12-13 13:58:47 +0200

    update files

--- clothesphonesapp/models.py
+++ clothesphonesapp/models.py
@@ -20,7 +20,7 @@ class Shop(models.Model):
     shop_phone      = models.CharField(max_length= 11)
     shop_location   = models.CharField(max_length= 220)
     num_of_share    = models.DecimalField(max_digits= 10 , decimal_places= 0, default= 0)
-    shop_image      = models.ImageField(upload_to = 'static/images' , default = None)
+    shop_image      = models.ImageField(upload_to = 'static' , default = None)
     class Meta:
         ordering = ['num_of_share']
 
@@ -36,7 +36,7 @@ class Product(models.Model):
     size    = models.CharField(max_length= 10 ,default= 'S' , help_text= "S,M,L,XL,XXL")
     price   = models.DecimalField(decimal_places = 2 , max_digits = 20, default= 0.00)
     describtion = models.TextField(null = True , help_text = 'this is optional if type is ACCESSORIES')
-    image = models.ImageField(upload_to = 'static/images', null= True)
+    image = models.ImageField(upload_to = 'static', null= True)
     shop_name = models.ForeignKey('Shop' , on_delete = models.CASCADE)
 
     class Meta:
@@ -54,53 +54,53 @@ class TextAdv(models.Model):
 
 
 class MediaAdv(models.Model):
-    homephoto1 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    homephoto2 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    homephoto3 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
+    homephoto1 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    homephoto2 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    homephoto3 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
 
-    home2photo1 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    home2photo2 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    home2photo3 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
+    home2photo1 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    home2photo2 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    home2photo3 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
 
-    cloth1photo1 = models.ImageField(upload_to="static/images" , default = None , null=True ,blank = True)
-    cloth1photo2 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    cloth1photo3 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-
-
-    cloth2photo1 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    cloth2photo2 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    cloth2photo3 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
+    cloth1photo1 = models.ImageField(upload_to="static" , default = None , null=True ,blank = True)
+    cloth1photo2 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    cloth1photo3 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
 
 
-    cloth3photo1 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    cloth3photo2 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    cloth3photo3 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
+    cloth2photo1 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    cloth2photo2 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    cloth2photo3 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
 
 
-    cloth4photo1 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    cloth4photo2 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    cloth4photo3 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
+    cloth3photo1 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    cloth3photo2 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    cloth3photo3 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+
+
+    cloth4photo1 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    cloth4photo2 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    cloth4photo3 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
 
 
 
-    phone1photo1 = models.ImageField(upload_to="static/images" , default = None , null=True ,blank = True)
-    phone1photo2 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    phone1photo3 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
+    phone1photo1 = models.ImageField(upload_to="static" , default = None , null=True ,blank = True)
+    phone1photo2 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    phone1photo3 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
 
 
-    phone2photo1 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    phone2photo2 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    phone2photo3 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
+    phone2photo1 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    phone2photo2 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    phone2photo3 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
 
 
-    phone3photo1 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    phone3photo2 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    phone3photo3 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
+    phone3photo1 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    phone3photo2 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    phone3photo3 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
 
 
-    phone4photo1 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    phone4photo2 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    phone4photo3 = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
+    phone4photo1 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    phone4photo2 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    phone4photo3 = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
 
-    cloth5photo = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
-    phone5photo = models.ImageField(upload_to="static/images" , default = None, null=True , blank = True)
+    cloth5photo = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
+    phone5photo = models.ImageField(upload_to="static" , default = None, null=True , blank = True)
